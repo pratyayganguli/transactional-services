e43a3ca5f250ce59a5d463c84070d37cf736d3eb
pratyay <pganguli5520@gmail.com>
2025-06-21 23:23:54 +0530

    Key splitter code added

--- transactional-services-sdk/build.gradle.kts
+++ transactional-services-sdk/build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok -- lombok annotation
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk18on -- bouncy castle for crypto operations.
+    implementation("org.bouncycastle:bcprov-jdk18on:1.81")
 }
 
 tasks.test {
--- transactional-services-sdk/src/test/java/shop/teener/transaction/KeyGeneratorTest.java
+++ transactional-services-sdk/src/test/java/shop/teener/transaction/KeyGeneratorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import javax.crypto.KeyGenerator;
 import java.security.Key;
 import java.security.Security;
+import java.util.Arrays;
 import java.util.Base64;
 
 /**
@@ -14,14 +15,16 @@ import java.util.Base64;
  * * @author Pratyay
  */
 public class KeyGeneratorTest {
-
+    // todo:
+    //  this is only for PoC purposes, all these test cases must be removed.
     @Test
     void generateAESKeyTest() {
-        generateAESKey();
+        String [] parts = generateAESKeyParts();
+        System.out.println(Arrays.toString(parts));
     }
-
     private static final int MAX_LENGTH = 256;
-    public void generateAESKey() {
+
+    public String[] generateAESKeyParts() {
         try {
             Security.addProvider(new BouncyCastleProvider());
             KeyGenerator keyGen = KeyGenerator.getInstance("AES", "BC");
@@ -29,9 +32,14 @@ public class KeyGeneratorTest {
             Key secretKey = keyGen.generateKey();
             byte[] keyBytes = secretKey.getEncoded();
             String b64Key = Base64.getEncoder().encodeToString(keyBytes);
-            System.out.printf("%s", b64Key);
-            System.out.println(b64Key.length());
+            System.out.println(b64Key);
+            int mid = b64Key.length()/2;
+            String[] parts = new String[2];
+            parts[0] = b64Key.substring(0, mid);
+            parts[1] = b64Key.substring(mid, b64Key.length());
+            return parts;
         } catch (Exception e) {
+            // use some kind of custom exception
             throw new RuntimeException("could not generate AES key");
         }
     }
